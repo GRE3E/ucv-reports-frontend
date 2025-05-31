59b19360e8686b2bb535acf98faed7294828cc27
Juan VML <jmunozlo01@ucvvirtual.edu.pe>
2025-05-31 03:18:06 -0500

    REGISTER OK

--- JAVASCRIPT/auth/register.js
+++ JAVASCRIPT/auth/register.js
@@ -5,3 +5,16 @@ window.addEventListener("load", () => {
     loading.style.display = "none";
   }, 1500); // 1.5 segundos
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const passwordInput = document.getElementById("verificarPassword");
+  const togglePassword = document.getElementById("toggleVerificarPassword");
+  const eyeIcon = togglePassword.querySelector("i");
+
+  togglePassword.addEventListener("click", function () {
+    const isPassword = passwordInput.type === "password";
+    passwordInput.type = isPassword ? "text" : "password";
+    eyeIcon.classList.toggle("fa-eye");
+    eyeIcon.classList.toggle("fa-eye-slash");
+  });
+});
