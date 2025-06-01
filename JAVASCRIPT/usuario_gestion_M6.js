7fc06bc68362be63d1a24d8a288d09a13bd359e7
GRE3E <84554352+GRE3E@users.noreply.github.com>
2025-06-01 02:15:29 -0500

    feat table boton edit user

--- JAVASCRIPT/usuario_gestion_M6.js
+++ JAVASCRIPT/usuario_gestion_M6.js
@@ -59,7 +59,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
       if (is_disabled_user) {
         actionsCell.innerHTML = `
-                  <button class="btn-action btn-edit" data-id="${user.IDUsuario}"><i class="fas fa-edit"></i>Editar</button>
                   <button class="btn-action btn-enable" data-id="${user.IDUsuario}"><i class="fas fa-user-plus"></i>Habilitar</button>
               `;
         actionsCell
@@ -67,6 +66,7 @@ document.addEventListener("DOMContentLoaded", () => {
           .addEventListener("click", () => enableUser(user.IDUsuario));
       } else {
         actionsCell.innerHTML = `
+                  <button class="btn-action btn-edit" data-id="${user.IDUsuario}"><i class="fas fa-edit"></i>Editar</button>
                   <button class="btn-action btn-disable" data-id="${user.IDUsuario}"><i class="fas fa-user-slash"></i>Deshabilitar</button>
               `;
         actionsCell
